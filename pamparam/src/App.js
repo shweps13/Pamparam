d87660c0ac3694acdffcf511bbcc6a298c254ba1
Heorhii Siburov <g.siburov@gmail.com>
2020-08-31 23:49:06 -0700

    Created -Post- component

--- pamparam/src/App.js
+++ pamparam/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.png'
 import './App.css';
+import Post from './Post.js';
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       <div className="app_header">
         <img src={logo} className="app_headerImage" alt='logo'/>
       </div>
-
+      <Post />
 
 
     </div>
