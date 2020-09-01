536e5fe63ca97df817e769880ebb3aa9b5d5ecdb
Heorhii Siburov <g.siburov@gmail.com>
2020-08-31 23:51:37 -0700

    Created -Post- component (file added)

---
+++ pamparam/src/Post.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Post() {
+    return (
+        <div>
+            {/* usrImg => usrName*/}
+
+            {/* main picture */}
+            
+            {/* picture text */}
+
+        </div>
+    )
+}
+
+export default Post
