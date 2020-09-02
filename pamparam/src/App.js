b59639166fd5b0d1775dde76f1fc4d91703f66c5
Heorhii Siburov <g.siburov@gmail.com>
2020-09-02 10:36:04 -0700

    import modal from MaterialUI

--- pamparam/src/App.js
+++ pamparam/src/App.js
@@ -3,29 +3,68 @@ import logo from './logo.png'
 import './App.css';
 import Post from './Post.js';
 import { db } from './firebase'
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+import { Button } from '@material-ui/core';
+
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: 'absolute',
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
 
 function App() {
+  const classes = useStyles();
+  const [modalStyle] = useState(getModalStyle);
 
   const [posts, setPosts] = useState([]);
+  const [open, setOpen] = useState(false);
 
   // Pool data from the Firebase DB
   useEffect(() => {
     db.collection('posts').onSnapshot(snapshot => {
       // pull data everytime when new post was added to the DB
-      setPosts(snapshot.docs.map(doc => doc.data()));
+      setPosts(snapshot.docs.map(doc => ({
+        id: doc.id,
+        post: doc.data()
+      })));
     })
   }, []);
 
   return (
     <div className="app">
 
+      <Modal open={open} onClose={() => setOpen(false)}>
+        <div style={modalStyle} className={classes.paper}>
+          <h3>Modal here</h3>
+        </div>
+      </Modal>
+
       <div className="app__header">
         <img src={logo} className="app__headerImage" alt="logo"/>
       </div>
       
+      <Button>Sign Up</Button>
+
       {
-        posts.map(post => (
-          <Post username={post.username} caption={post.caption} imageUrl={post.imageUrl} />
+        posts.map(({ id, post }) => (
+          <Post key={id} username={post.username} caption={post.caption} imageUrl={post.imageUrl} />
         ))
       }
 
