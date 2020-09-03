09a3c137c71b70354022b63336fbe211028129e7
Heorhii Siburov <g.siburov@gmail.com>
2020-09-02 21:01:21 -0700

    function for visualisation of file uploading

--- pamparam/src/components/ImageUpload.js
+++ pamparam/src/components/ImageUpload.js
@@ -1,11 +1,13 @@
-import React, { useState } from 'react'
-import { Button } from '@material-ui/core'
+import React, { useState } from 'react';
+import { Button } from '@material-ui/core';
+import { db, storage } from '../materials/firebase.js';
 
 function ImageUpload() {
     const [caption, setCaption] = useState('');
     const [progress, setProgress] = useState(0);
     const [image, setImage] = useState('');
 
+    // get the first file that was selected and put in to the hook
     const handleChange = (e) => {
         if (e.target.files[0]) {
             setImage(e.target.files[0]);
@@ -13,7 +15,20 @@ function ImageUpload() {
     }
 
     const handleUpload = (e) => {
-        
+        // get reference from firebase and put there image
+        const uploadTask = storage.ref(`images/${image.name}`).put(image);
+
+        uploadTask.on(
+            'state_changed',
+            (snapshot) => {
+                // progress function
+                const progress = Math.round(
+                    (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+                );
+                setProgress(progress);
+            }
+        )
+
     }
 
     return (
