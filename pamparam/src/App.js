e1a914f2a9d694384687b6bc46830018134fe6c3
Heorhii Siburov <g.siburov@gmail.com>
2020-09-02 18:22:24 -0700

    code reformation

--- pamparam/src/App.js
+++ pamparam/src/App.js
@@ -1,38 +1,16 @@
 import React, { useState, useEffect } from 'react';
-import logo from './logo.png'
+import logo from './materials/logo.png'
 import './App.css';
-import Post from './Post.js';
-import { db, auth } from './firebase'
-import { makeStyles } from '@material-ui/core/styles';
+import Post from './components/Post.js';
+import { db, auth } from './materials/firebase'
 import Modal from '@material-ui/core/Modal';
 import { Button, Input } from '@material-ui/core';
+import { getModalStyle, useStyles } from './materials/modalStyles.js';
 
-function getModalStyle() {
-  const top = 50;
-  const left = 50;
-
-  return {
-    top: `${top}%`,
-    left: `${left}%`,
-    transform: `translate(-${top}%, -${left}%)`,
-  };
-}
-
-const useStyles = makeStyles((theme) => ({
-  paper: {
-    position: 'absolute',
-    width: 400,
-    backgroundColor: theme.palette.background.paper,
-    border: '2px solid #000',
-    boxShadow: theme.shadows[5],
-    padding: theme.spacing(2, 4, 3),
-  },
-}));
 
 function App() {
   const classes = useStyles();
   const [modalStyle] = useState(getModalStyle);
-
   const [posts, setPosts] = useState([]);
   const [open, setOpen] = useState(false);
   const [email, setEmail] = useState('');
---
+++ pamparam/src/materials/modalStyles.js
@@ -0,0 +1,25 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+function getModalStyle() {
+    const top = 50;
+    const left = 50;
+  
+    return {
+      top: `${top}%`,
+      left: `${left}%`,
+      transform: `translate(-${top}%, -${left}%)`,
+    };
+  }
+  
+  const useStyles = makeStyles((theme) => ({
+    paper: {
+      position: 'absolute',
+      width: 400,
+      backgroundColor: theme.palette.background.paper,
+      border: '2px solid #000',
+      boxShadow: theme.shadows[5],
+      padding: theme.spacing(2, 4, 3),
+    },
+  }));
+
+export { getModalStyle, useStyles };
