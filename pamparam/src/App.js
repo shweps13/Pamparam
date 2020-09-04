de6990368535e7a19aa6ad34fd298f6c57d9052d
Heorhii Siburov <g.siburov@gmail.com>
2020-09-04 01:15:33 -0700

    added some icons for future switch

--- pamparam/src/App.js
+++ pamparam/src/App.js
@@ -12,6 +12,10 @@ import { getModalStyle, useStyles } from './materials/modalStyles.js';
 import { auth } from './materials/firebase'
 import logo from './materials/logo.png'
 
+import { RiSendPlaneLine, RiSendPlaneFill } from 'react-icons/ri';
+import { BsPlusCircle, BsPlusCircleFill } from 'react-icons/bs';
+import { AiOutlineHome, AiFillHome, AiOutlineHeart, AiFillHeart} from 'react-icons/ai';
+
 function App() {
   const classes = useStyles();
   const [modalStyle] = useState(getModalStyle);
@@ -79,12 +83,16 @@ function App() {
     <Router>
       <div className="app">
   
-        <div className="main__header">
+        <div className="app__header">
         
           <ModalSignup open={open} setOpen={setOpen} modalStyle={modalStyle} classesStyle={classes.paper} username={username} setUsername={setUsername} email={email} setEmail={setEmail} password={password} setPassword={setPassword} signUp={signUp} />
           <ModalSignin openSignIn={openSignIn} setOpenSignIn={setOpenSignIn} modalStyle={modalStyle} classesStyle={classes.paper} email={email} setEmail={setEmail} password={password} setPassword={setPassword} signIn={signIn} />
           
-          <img src={logo} className="main__headerImage" alt="logo"/>
+          <img src={logo} className="app__headerImage" alt="logo"/>
+          <div>
+            <RiSendPlaneLine size={25}/> <RiSendPlaneFill size={25}/>
+            <AiOutlineHome size={25}/> <AiFillHome size={25}/> <AiOutlineHeart size={25}/> <AiFillHeart size={25}/> <BsPlusCircle size={25}/> <BsPlusCircleFill size={25}/>
+          </div>
 
         {user ? (
           <Button onClick={() => auth.signOut()}>Logout</Button>
