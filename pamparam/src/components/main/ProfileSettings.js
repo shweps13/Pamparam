11ec0c30061aeed9116f34986eea616f50e08ff9
Heorhii Siburov <g.siburov@gmail.com>
2020-09-09 20:27:37 -0700

    CircularProgressBar was added to fix froblem with fetching user data

--- pamparam/src/components/main/ProfileSettings.js
+++ pamparam/src/components/main/ProfileSettings.js
@@ -3,6 +3,7 @@ import userPic from '../../materials/loginPlease.jpg';
 import '../../styles/ProfileSettings.css';
 
 import Grid from '@material-ui/core/Grid';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 
 function ProfileSettings({user}) {
@@ -13,169 +14,178 @@ function ProfileSettings({user}) {
 
     return (
     <div className="profileSet">
-        <Grid container spacing={2}>
-            <Grid className="profileSet__header" item xs={12}>
-                <img src={userPic} className="profileSet__userImage" alt="logo"/>
-                <div>
-                    <h2>barsik</h2>
-                    <p>Change user avatar</p>
+
+        {user === null ? (
+                <div className="profileSet__comments">
+                    <CircularProgress size={100} />
                 </div>
-            </Grid>
-            <Grid className="profileSet__leftColumn" item xs={5}>
-                <label>User name</label>
-            </Grid>
-            <Grid className="profileSet__rightColumn" item xs={7}>
-                <div className="profileSet__rightBlock">
-                    <input
-                    placeholder="Username"
-                    type="text"
-                    value={newUsername}
-                    onChange={(e) => setNewusername(e.target.value)}
-                    />
-                </div>
-            </Grid>
+            ): (
+                <Grid container spacing={2}>
+                    <Grid className="profileSet__header" item xs={12}>
+                        <img src={userPic} className="profileSet__userImage" alt="logo"/>
+                        <div>
+                            <h2>barsik</h2>
+                            <p>Change user avatar</p>
+                        </div>
+                    </Grid>
+                    <Grid className="profileSet__leftColumn" item xs={5}>
+                        <label>User name</label>
+                    </Grid>
+                    <Grid className="profileSet__rightColumn" item xs={7}>
+                        <div className="profileSet__rightBlock">
+                            <input
+                            placeholder="Username"
+                            type="text"
+                            value={newUsername}
+                            onChange={(e) => setNewusername(e.target.value)}
+                            />
+                        </div>
+                    </Grid>
 
-            <Grid className="profileSet__leftColumn" item xs={5}>
-                <label>Full name</label>
-            </Grid>
-            <Grid className="profileSet__rightColumn" item xs={7}>
-                <div className="profileSet__rightBlock">
-                    <input
-                    placeholder="Username"
-                    type="text"
-                    value={newUsername}
-                    onChange={(e) => setNewusername(e.target.value)}
-                    />
-                    <p>Help people discover your account by using the name you're known by: either your full name, nickname, or business name.</p>
-                </div>
-            </Grid>
+                    <Grid className="profileSet__leftColumn" item xs={5}>
+                        <label>Full name</label>
+                    </Grid>
+                    <Grid className="profileSet__rightColumn" item xs={7}>
+                        <div className="profileSet__rightBlock">
+                            <input
+                            placeholder="Username"
+                            type="text"
+                            value={newUsername}
+                            onChange={(e) => setNewusername(e.target.value)}
+                            />
+                            <p>Help people discover your account by using the name you're known by: either your full name, nickname, or business name.</p>
+                        </div>
+                    </Grid>
 
-            <Grid className="profileSet__leftColumn" item xs={5}>
-                <label>Webpage</label>
-            </Grid>
-            <Grid className="profileSet__rightColumn" item xs={7}>
-                <input
-                placeholder="Username"
-                type="text"
-                value={newUsername}
-                onChange={(e) => setNewusername(e.target.value)}
-                />
-            </Grid>
+                    <Grid className="profileSet__leftColumn" item xs={5}>
+                        <label>Webpage</label>
+                    </Grid>
+                    <Grid className="profileSet__rightColumn" item xs={7}>
+                        <input
+                        placeholder="Username"
+                        type="text"
+                        value={newUsername}
+                        onChange={(e) => setNewusername(e.target.value)}
+                        />
+                    </Grid>
 
-            <Grid className="profileSet__leftColumn" item xs={5}>
-                <label>Bio</label>
-            </Grid>
-            <Grid className="profileSet__rightColumn" item xs={7}>
-                <div className="profileSet__rightBlock">
-                    <textarea
-                    placeholder="Username"
-                    type="text"
-                    value={newUsername}
-                    onChange={(e) => setNewusername(e.target.value)}
-                    rows="3"
-                    />
-                    <strong>Personal Information</strong>
-                </div>
-            </Grid>
+                    <Grid className="profileSet__leftColumn" item xs={5}>
+                        <label>Bio</label>
+                    </Grid>
+                    <Grid className="profileSet__rightColumn" item xs={7}>
+                        <div className="profileSet__rightBlock">
+                            <textarea
+                            placeholder="Username"
+                            type="text"
+                            value={newUsername}
+                            onChange={(e) => setNewusername(e.target.value)}
+                            rows="3"
+                            />
+                            <strong>Personal Information</strong>
+                        </div>
+                    </Grid>
 
-            <Grid className="profileSet__leftColumn" item xs={5}>
-                <label>Email</label>
-            </Grid>
-            <Grid className="profileSet__rightColumn" item xs={7}>
-                <input
-                placeholder="Username"
-                type="text"
-                value={newUsername}
-                onChange={(e) => setNewusername(e.target.value)}
-                />
-            </Grid>
+                    <Grid className="profileSet__leftColumn" item xs={5}>
+                        <label>Email</label>
+                    </Grid>
+                    <Grid className="profileSet__rightColumn" item xs={7}>
+                        <input
+                        placeholder="Username"
+                        type="text"
+                        value={newUsername}
+                        onChange={(e) => setNewusername(e.target.value)}
+                        />
+                    </Grid>
 
-            <Grid className="profileSet__leftColumn" item xs={5}>
-                <label>Phone number</label>
-            </Grid>
-            <Grid className="profileSet__rightColumn" item xs={7}>
-                <input
-                placeholder="Username"
-                type="text"
-                value={newUsername}
-                onChange={(e) => setNewusername(e.target.value)}
-                />
-            </Grid>
+                    <Grid className="profileSet__leftColumn" item xs={5}>
+                        <label>Phone number</label>
+                    </Grid>
+                    <Grid className="profileSet__rightColumn" item xs={7}>
+                        <input
+                        placeholder="Username"
+                        type="text"
+                        value={newUsername}
+                        onChange={(e) => setNewusername(e.target.value)}
+                        />
+                    </Grid>
 
-            <Grid className="profileSet__leftColumn" item xs={5}>
-                <label>Gender</label>
-            </Grid>
-            <Grid className="profileSet__rightColumn" item xs={7}>
-                <input
-                placeholder="Username"
-                type="text"
-                value={newUsername}
-                onChange={(e) => setNewusername(e.target.value)}
-                />
-            </Grid>
-            <Grid className="profileSet__leftColumn" item xs={5} />
-            <Grid className="profileSet__rightColumn" item xs={7}>
-                <button>Submit</button>   
-            </Grid>
+                    <Grid className="profileSet__leftColumn" item xs={5}>
+                        <label>Gender</label>
+                    </Grid>
+                    <Grid className="profileSet__rightColumn" item xs={7}>
+                        <input
+                        placeholder="Username"
+                        type="text"
+                        value={newUsername}
+                        onChange={(e) => setNewusername(e.target.value)}
+                        />
+                    </Grid>
+                    <Grid className="profileSet__leftColumn" item xs={5} />
+                    <Grid className="profileSet__rightColumn" item xs={7}>
+                        <button>Submit</button>   
+                    </Grid>
 
-            <Grid className="profileSet__leftColumn" item xs={5}>
-                <label>Old password</label>
-            </Grid>
-            <Grid className="profileSet__rightColumn" item xs={7}>
-                <input
-                placeholder="Username"
-                type="text"
-                value={newUsername}
-                onChange={(e) => setNewusername(e.target.value)}
-                />
-            </Grid>
-            <Grid className="profileSet__leftColumn" item xs={5}>
-                <label>New password</label>
-            </Grid>
-            <Grid className="profileSet__rightColumn" item xs={7}>
-                <input
-                placeholder="Username"
-                type="text"
-                value={newUsername}
-                onChange={(e) => setNewusername(e.target.value)}
-                />
-            </Grid>
-            <Grid className="profileSet__leftColumn" item xs={5}>
-                <label>Confirm new password</label>
-            </Grid>
-            <Grid className="profileSet__rightColumn" item xs={7}>
-                <input
-                placeholder="Username"
-                type="text"
-                value={newUsername}
-                onChange={(e) => setNewusername(e.target.value)}
-                />
-            </Grid>
+                    <Grid className="profileSet__leftColumn" item xs={5}>
+                        <label>Old password</label>
+                    </Grid>
+                    <Grid className="profileSet__rightColumn" item xs={7}>
+                        <input
+                        placeholder="Username"
+                        type="text"
+                        value={newUsername}
+                        onChange={(e) => setNewusername(e.target.value)}
+                        />
+                    </Grid>
+                    <Grid className="profileSet__leftColumn" item xs={5}>
+                        <label>New password</label>
+                    </Grid>
+                    <Grid className="profileSet__rightColumn" item xs={7}>
+                        <input
+                        placeholder="Username"
+                        type="text"
+                        value={newUsername}
+                        onChange={(e) => setNewusername(e.target.value)}
+                        />
+                    </Grid>
+                    <Grid className="profileSet__leftColumn" item xs={5}>
+                        <label>Confirm new password</label>
+                    </Grid>
+                    <Grid className="profileSet__rightColumn" item xs={7}>
+                        <input
+                        placeholder="Username"
+                        type="text"
+                        value={newUsername}
+                        onChange={(e) => setNewusername(e.target.value)}
+                        />
+                    </Grid>
 
-            <Grid className="profileSet__leftColumn" item xs={5} />
-            <Grid className="profileSet__rightColumn" item xs={7}>
-                <div className="profileSet__changePass">
-                    <button>Change</button>   
-                    <button>Forgot password?</button>   
-                </div>
-            </Grid>
+                    <Grid className="profileSet__leftColumn" item xs={5} />
+                    <Grid className="profileSet__rightColumn" item xs={7}>
+                        <div className="profileSet__changePass">
+                            <button>Change</button>   
+                            <button>Forgot password?</button>   
+                        </div>
+                    </Grid>
 
-            <Grid className="profileSet__leftColumn" item xs={5}>
-            </Grid>
-            <Grid className="profileSet__rightColumn" item xs={7}>
-                <div className="profileSet__rightBlock">
-                    <strong>Account removal</strong>
-                    <p>You can delete it and nobody will get your personal data anymore.</p>
-                </div>
-            </Grid>
-            <Grid className="profileSet__leftColumn" item xs={5}>
-                <label>Delete your account</label>
-            </Grid>
-            <Grid className="profileSet__rightColumn" item xs={7}>
-                <button>Delete</button>   
-            </Grid>
+                    <Grid className="profileSet__leftColumn" item xs={5}>
+                    </Grid>
+                    <Grid className="profileSet__rightColumn" item xs={7}>
+                        <div className="profileSet__rightBlock">
+                            <strong>Account removal</strong>
+                            <p>You can delete it and nobody will get your personal data anymore.</p>
+                        </div>
+                    </Grid>
+                    <Grid className="profileSet__leftColumn" item xs={5}>
+                        <label>Delete your account</label>
+                    </Grid>
+                    <Grid className="profileSet__rightColumn" item xs={7}>
+                        <button>Delete</button>   
+                    </Grid>
 
-        </Grid>
+                </Grid>
+            )}
+
+        
         
     </div>
     )
