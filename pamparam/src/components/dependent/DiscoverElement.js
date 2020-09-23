eb721307e63f5da07422e373dadc1a24ed3688e6
Heorhii Siburov <g.siburov@gmail.com>
2020-09-23 01:18:46 -0700

    nice squares finally fixed in grid of [Discover] !!!

--- pamparam/src/components/dependent/DiscoverElement.js
+++ pamparam/src/components/dependent/DiscoverElement.js
@@ -6,7 +6,7 @@ function DiscoverElement({ id, imageUrl, alt }) {
     return (
     <div className="discover__image">
         <div className="discover__sideCrop">
-            <img src={imageUrl} className="discover__image" alt={`${alt}'s post`} />
+            <img src={imageUrl} alt={`${alt}'s post`} />
         </div>
     </div>
     )
