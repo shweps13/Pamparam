1fa2f3f73563ebceb6578941baf77ae622ee1540
Heorhii Siburov <g.siburov@gmail.com>
2020-09-23 17:39:11 -0700

    [discover] gallery fixed for all browsers, added hover for grid elements

--- pamparam/src/components/dependent/DiscoverElement.js
+++ pamparam/src/components/dependent/DiscoverElement.js
@@ -2,11 +2,14 @@ import React from 'react';
 
 function DiscoverElement({ id, imageUrl, alt }) {
 
+    const styles = {
+        backgroundImage: `url(${imageUrl})`
+      };
 
     return (
     <div className="discover__image">
-        <div className="discover__sideCrop">
-            <img src={imageUrl} alt={`${alt}'s post`} />
+        <div style={styles} className="discover__sideCrop">
+            {/* <img src={imageUrl} alt={`${alt}'s post`} /> */}
         </div>
     </div>
     )
