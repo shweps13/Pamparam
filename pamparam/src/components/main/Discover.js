1ec5cb40b70e4e7a3958c6f0c386dca249fb5c83
Heorhii Siburov <g.siburov@gmail.com>
2020-09-30 18:33:00 -0700

    reformat of grid to mobile view

--- pamparam/src/components/main/Discover.js
+++ pamparam/src/components/main/Discover.js
@@ -10,9 +10,6 @@ import { getModalStyle, useDiscoverStyles } from '../../materials/modalStyles.js
 import { db } from '../../materials/firebase';
 
 // All next parts for [Discover] feature 
-// ==> Post parts in modal
-// ==> Ability to get render info and comments
-// ==> Ability to add comments
 // ==> Shuffle mixed styles alorithm
 // ==> lazy load
 // ==> mobile view / @media stylings
