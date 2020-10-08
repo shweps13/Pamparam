d006f23fde4a9316ee04eb53d5775a20ed11e760
Heorhii Siburov <g.siburov@gmail.com>
2020-10-07 18:14:38 -0700

    modal header fixed

--- pamparam/src/components/dependent/ModalNewMessage.js
+++ pamparam/src/components/dependent/ModalNewMessage.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Modal from '@material-ui/core/Modal';
 import '../../styles/Modal.css';
 
+import { GrClose } from 'react-icons/gr';
+
 
 function ModalNewMessage({ modalMessage, setModalMessage, modalStyle, classesStyle }) {
 
@@ -12,10 +14,13 @@ function ModalNewMessage({ modalMessage, setModalMessage, modalStyle, classesSty
             <div className="modalMessage__body">
                 <div className="modalMessage__body__header">
                     <div className="modalMessage__body__header__close">
+                        <GrClose size={20} onClick={() => setModalMessage(false)} />
                     </div>      
                     <div className="modalMessage__body__header__title">
+                        <h3>New Message</h3>
                     </div>      
                     <div className="modalMessage__body__header__next">
+                        <button>Next</button>
                     </div>      
                 </div>      
                 
