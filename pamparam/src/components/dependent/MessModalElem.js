d0f044ac1ad7a133f3c7501d86374f2bdf7ac56c
Heorhii Siburov <g.siburov@gmail.com>
2020-10-12 12:25:10 -0700

    checkboxes implemented

--- pamparam/src/components/dependent/MessModalElem.js
+++ pamparam/src/components/dependent/MessModalElem.js
@@ -5,20 +5,24 @@ function MessModalElem({ displayName, fullName }) {
 
     return (
         <div className="MessModalElem">
-            <img src={noAvatar} alt='user avatar' />
+            <div className="MessModalElem__Left">
+                <img src={noAvatar} alt='user avatar' />
 
-            {fullName ? (
-                <div className="MessModalElem__body">
-                    <strong>{displayName.toLowerCase()}</strong>
-                    <p>{fullName}</p>
-                </div>
-            ):(
-                <div className="MessModalElem__body__noFull">
-                    <strong>{displayName.toLowerCase()}</strong>
-                </div>
-            )}
-
-            
+                {fullName ? (
+                    <div className="MessModalElem__body">
+                        <strong>{displayName.toLowerCase()}</strong>
+                        <p>{fullName}</p>
+                    </div>
+                ):(
+                    <div className="MessModalElem__body__noFull">
+                        <strong>{displayName.toLowerCase()}</strong>
+                    </div>
+                )}
+            </div>
+            <label className="MessModalElem__checkbox__div">
+                <input type="checkbox"></input>
+                <span className="MessModalElem__checkmark"></span>
+            </label>
         </div>
     )
 }
