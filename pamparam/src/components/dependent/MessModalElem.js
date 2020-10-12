b6ca33c19dceff49fbfe3b72d7f48b655fc057cd
Heorhii Siburov <g.siburov@gmail.com>
2020-10-12 11:55:58 -0700

    styling elements are ready, continue to works with selection

--- pamparam/src/components/dependent/MessModalElem.js
+++ pamparam/src/components/dependent/MessModalElem.js
@@ -6,10 +6,19 @@ function MessModalElem({ displayName, fullName }) {
     return (
         <div className="MessModalElem">
             <img src={noAvatar} alt='user avatar' />
-            <div className="MessModalElem__body">
-                <strong>{displayName}</strong>
-                <p>{fullName}</p>
-            </div>
+
+            {fullName ? (
+                <div className="MessModalElem__body">
+                    <strong>{displayName.toLowerCase()}</strong>
+                    <p>{fullName}</p>
+                </div>
+            ):(
+                <div className="MessModalElem__body__noFull">
+                    <strong>{displayName.toLowerCase()}</strong>
+                </div>
+            )}
+
+            
         </div>
     )
 }
