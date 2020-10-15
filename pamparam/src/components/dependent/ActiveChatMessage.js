726f118e1e9da0c315a1c7e276c6e3bcbcfe87f9
Heorhii Siburov <g.siburov@gmail.com>
2020-10-14 18:01:52 -0700

    users now able to send big red hearts

--- pamparam/src/components/dependent/ActiveChatMessage.js
+++ pamparam/src/components/dependent/ActiveChatMessage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import noAvatar from '../../materials/noAvatar.jpg'; 
 
+import { BsFillHeartFill } from 'react-icons/bs';
 
 function ActiveChatMessage({ currentUser, message, userId, timestamp }) {
 
@@ -17,17 +18,29 @@ function ActiveChatMessage({ currentUser, message, userId, timestamp }) {
         {isLocalMessage() === true ? (
             <div className="activeChatMessage__content">
                 <div/>
-                <div>
-                    <p>{message}</p>
-                </div>
+                {message === '<3' ? (
+                    <div className="activeChatMessage__heart">
+                        <BsFillHeartFill size={50} />
+                    </div>
+                ):(
+                    <div>
+                        <p>{message}</p>
+                    </div>
+                )}
             </div>
         ):(
             <div className="activeChatMessage__content__remoteUser">
                 <img src={noAvatar} alt='user avatar' />
                 <div/>
-                <div>
-                    <p>{message}</p>
-                </div>
+                {message === '<3' ? (
+                    <div className="activeChatMessage__heart__remoteUser">
+                        <BsFillHeartFill size={50}/>
+                    </div>
+                ):(
+                    <div>
+                        <p>{message}</p>
+                    </div>
+                )}
             </div>
         )}
     </div>
